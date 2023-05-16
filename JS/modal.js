70f31f4e6bc69c4fda35f9a4a5efd9628a45ed81
TimCauss <t.caussignac@gmail.com>
2023-05-16 13:14:44 +0200

    style & feat: changing buttons style. Adding and deleting bulb works

--- JS/modal.js
+++ JS/modal.js
@@ -1,18 +1,33 @@
 const modalContainer = document.querySelector(".modal-container");
 const modalTrigger = document.querySelectorAll(".modal-trigger");
-const modalContainer2 = document.querySelector(".modal-container2")
-const modalTrigger2 = document.querySelectorAll(".modal-trigger2")
+const modalContainer2 = document.querySelector(".modal-container2");
+const modalTrigger2 = document.querySelectorAll(".modal-trigger2");
+const deleteBtn = document.querySelector(".delete");
+
+let id = null;
 
 modalTrigger.forEach((trigger) =>
-    trigger.addEventListener("click", toggleModal)
+  trigger.addEventListener("click", toggleModal)
 );
-modalTrigger2.forEach(trigger =>
-    trigger.addEventListener("click", toggleModal2)
+modalTrigger2.forEach((trigger) =>
+  trigger.addEventListener("click", () => {
+    id = trigger.getAttribute("data-id");
+    toggleModal2();
+  })
 );
 
+deleteBtn.addEventListener("click", () => {
+  goto("del");
+});
+
 function toggleModal() {
-    modalContainer.classList.toggle("active");
+  modalContainer.classList.toggle("active");
 }
 function toggleModal2() {
-    modalContainer2.classList.toggle("active")
+  modalContainer2.classList.toggle("active");
+}
+
+function goto(str) {
+    if (str === "del") window.location.replace("del.php?id=" + id);
+
 }
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 
 
+
 require_once("connect.php");
 
 $sql = "SELECT * FROM ampoules";
@@ -60,6 +61,7 @@ require_once("close.php")
         <table>
 
             <thead>
+                <th>ID</th>
                 <th>Date</th>
                 <th>Stage</th>
                 <th>Position</th>
@@ -67,39 +69,48 @@ require_once("close.php")
             </thead>
 
             <tbody>
-                <?php
-                foreach ($result as $row) {
-                ?>
-                    <tr>
-                        <td>
-                            <?php
-                                $format_date = new DateTime($row["create_date"]);
-                                $change_date = $format_date->format('d/m/Y');
-                                echo $change_date;
-                            ?>
-                        </td>
-                        <td><?= $row["stage"] ?></td>
-                        <td><?= $row["position"] ?></td>
-                        <td><?= $row["price"] . " $" ?></td>
-                        <td>
-                                <svg class="modal-trigger2" width="11" height="13" viewBox="0 0 11 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <form class="edit-form" method="POST">
+                    <?php
+                    foreach ($result as $row) {
+                    ?>
+                        <tr>
+                            <td class="td-hover td-small"><?= $row["id"] ?></td>
+                            <td class="td-hover">
+                                <input class="edit_input" type="date" name="edit_date" value='<?= $row["create_date"] ?>'>
+                            </td>
+                            <td class="td-hover td-small"><input class="edit_input" name="edit_stage" type="number" min="0" max="8" value="<?= $row['stage'] ?>"> </td>
+                            <td class="td-hover td-position">
+                                <select class="edit_input" name="edit_position">
+                                    <option value=""><?= $row["position"] ?></option>
+                                    <option value="North">North</option>
+                                    <option value="South">South</option>
+                                    <option value="East">East</option>
+                                    <option value="West">West</option>
+                                </select>
+                            </td>
+                            <td class="td-hover td-small">
+                                <input class="edit_input" type="text" name="edit_price" value='<?= $row["price"] . " $" ?>'>
+                            </td>
+                            <td class="td-icon">
+                                <svg class="modal-trigger2" data-id="<?= $row["id"] ?>" width="11" height="13" viewBox="0 0 11 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M4.78571 0.785713C4 0.785713 3.35714 1.42857 3.35714 2.21428H1.92857C1.14286 2.21428 0.5 2.85714 0.5 3.64286H10.5C10.5 2.85714 9.85714 2.21428 9.07143 2.21428H7.64286C7.64286 1.42857 7 0.785713 6.21429 0.785713H4.78571ZM1.92857 5.07143V11.9429C1.92857 12.1 2.04286 12.2143 2.2 12.2143H8.81429C8.97143 12.2143 9.08571 12.1 9.08571 11.9429V5.07143H7.65714V10.0714C7.65714 10.4714 7.34286 10.7857 6.94286 10.7857C6.54286 10.7857 6.22857 10.4714 6.22857 10.0714V5.07143H4.8V10.0714C4.8 10.4714 4.48571 10.7857 4.08571 10.7857C3.68571 10.7857 3.37143 10.4714 3.37143 10.0714V5.07143H1.94286H1.92857Z" />
                                 </svg>
-                            <a href="edit.php?id=<?= $row["id"] ?>">
-                                <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M8 0.5L6.75 1.75L9.25 4.25L10.5 3L8 0.5ZM5.5 3L0.5 8V10.5H3L8 5.5L5.5 3Z" />
+
+                                <svg width="20" height="20" class="modal-trigger3" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M20.048 6.352a1.2 1.2 0 0 1 0 1.696l-9.6 9.6a1.2 1.2 0 0 1-1.696 0l-4.8-4.8a1.2 1.2 0 0 1 1.696-1.696L9.6 15.103l8.752-8.751a1.2 1.2 0 0 1 1.696 0Z" clip-rule="evenodd"></path>
                                 </svg>
-                            </a>
-                        </td>
-                    </tr>
-                <?php
-                }
-                ?>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </form>
             </tbody>
         </table>
     </main>
 
     <!-- ! Début modal -->
+    <?php ?>
     <div class="modal-container">
         <div class="overlay modal-trigger"></div>
         <div class="modal">
@@ -143,7 +154,8 @@ require_once("close.php")
                 <p>Etes vous sûr de vouloir supprimer cette entrée ?</p>
             </div>
             <div class="modal-btn-ctn">
-                <div class="btn-suppr"><a href="del.php?id=<?=$row["id"]?>"><button>Supprimer</button</a></div>
+                <div class="btn-suppr"><button class="delete">Supprimer</button>
+                </div>
                 <div class="btn-cancel modal-trigger2"><button>Cancel</button></div>
             </div>
         </div>
