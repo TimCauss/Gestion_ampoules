d3474ea3c6656927da3ab5986b6afc3dc4e3c1ac
TimCauss <t.caussignac@gmail.com>
2023-05-17 16:55:56 +0200

    resgistration is now done and login page is started

--- inscription.php
+++ inscription.php
@@ -1,26 +1,40 @@
 <?php
 
 //on vérifie si le formulaire a été envoyé
-if(!empty($_POST)) {
+if (!empty($_POST)) {
     //formulaire envoé
     //On vérifie les champs requis
-    if(isset($_POST["pseudo"], $_POST["email"], $_POST["password"])
-    && !empty($_POST["pseudo"]) && !empty($_POST["email"]) && !empty($_POST["password"])
+    if (
+        isset($_POST["user_name"], $_POST["email"], $_POST["pass"])
+        && !empty($_POST["user_name"]) && !empty($_POST["email"]) && !empty($_POST["pass"])
     ) {
         //Le formulaire est complet
         //on récupère les données en les protégeant
-        $user_name = strip_tags($_POST["pseudo"]);
+        $user_name = strip_tags($_POST["user_name"]);
 
         //On vérifie si l'email est bien un email :
-            if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-                die("Mail address incorrect");
-            }
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            die("Mail address incorrect");
+        }
 
-            //on va hasher le mot de passe
-            $password = password_hash($_POST["password"], PASSWORD_ARGON2ID);
-            
-    
-        } else {
+        //on va hasher le mot de passe
+        $pass = password_hash($_POST["pass"], PASSWORD_ARGON2ID);
+
+        //on enregistre en base de donnée
+        require_once "connect.php";
+
+        $sql = "INSERT INTO users(user_name, email, pass)
+            VALUES (:user_name, :email, '$pass')";
+
+        $query = $db->prepare($sql);
+        $query->bindValue(":user_name", $user_name);
+        $query->bindValue(":email", $_POST["email"]);
+        $query->execute();
+
+        echo '<script language="Javascript">
+            alert ("User created" )
+            </script>';
+    } else {
         die("Formulaire incomplet");
     }
 }
@@ -30,18 +44,18 @@ if(!empty($_POST)) {
 
 <h1>Inscription</h1>
 
-<form method="POST" >
+<form method="POST">
     <div>
-        <label for="pseudo">Pseudo</label>
-        <input type="text" name="user_name" id="pseudo">
+        <label for="user_name">Username</label>
+        <input type="text" name="user_name" id="user_name">
     </div>
     <div>
         <label for="email">Email</label>
         <input type="email" name="email" id="email">
     </div>
     <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
+        <label for="pass">Password</label>
+        <input type="password" name="pass" id="pass">
     </div>
     <button type="submit">Create user</button>
 </form>
--- login.php
+++ login.php
@@ -1,3 +1,39 @@
+<?php
+
+//on vérifie si le formulaire a été envoyé
+if (!empty($_POST)) {
+    //formulaire envoyé
+    //On vérifie les champs requis
+    if (
+        isset($_POST["user_name"], $_POST["pass"])
+        && !empty($_POST["user_name"]) && !empty($_POST["pass"])
+    ) {
+        //Le formulaire est complet
+        //on récupère les données en les protégeant
+        $user_name = strip_tags($_POST["user_name"]);
+
+        //on se connecte à la BDD
+        require_once "connect.php";
+        $sql = "SELECT user_name, pass FROM users WHERE user_name = :user_name";
+        $query = $db->prepare($sql);
+        $query->bindValue(":user_name", $user_name, PDO::PARAM_STR);
+        $query->execute();
+
+        $user = $query->fetch();
+
+        if(!$user){
+            die("User does not exist");
+        }
+        if(!password_verify($_POST["pass"], $user["pass"])){
+            die();
+        }
+
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,17 +53,17 @@
                     <h1>need help ?</h1>
                 </a>
             </div>
-            <form action="">
+            <form method="POST">
                 <div class="form-main">
                     <div>
                         <input type="text" name="user_name" id="user_name" placeholder="Username">
                     </div>
                     <div>
-                        <input type="password" name="password" id="password" placeholder="Password">
+                        <input type="password" name="pass" id="pass" placeholder="Password">
                     </div>
                     <div class="action-ctn">
                         <div class="subm-ctn">
-                            <input type="submit" value="Login">
+                            <button type="submit">login</button>
                         </div>
                         <div class="subm-ctn forgt">
                             <a href="#">Forgot Password ?</a>
